04faf3fabd4782c70e8e786e629f20fb87a5258e
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 15:18:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ end
 def kesha_maker(array)
   new_array = []
   array.collect do |x|
-  new_array << "#{x.insert(2,"$")}"
+  new_array << "#{x.insert(2,"$").delete_at(3)}"
   end
   new_array
 end
