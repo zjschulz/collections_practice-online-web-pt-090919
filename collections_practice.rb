eeb67d5cddf71bd035dcd67a61aef44bd87a2876
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 15:29:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,14 +22,9 @@ end
 
 def kesha_maker(array)
   new_array = []
-  another_array = []
   array.collect do |x|
-  new_array << "#{x.insert(2,"$")}"
+  new_array << "#{x.insert(2,"$").slice!(3)}"
   end
-  new_array.collect do |y|
-  another_array << "#{y}"
-  end
-  another_array
 end
 
 def find_a(array)
