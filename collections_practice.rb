194cabb0aeb6ffb16e7ccc7c51009e4d5d80d664
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 15:15:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b| a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.insert(1,array[2]).delete_at(3)
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |x|
+  new_array << "#{x}"
+  end
+  new_array
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  new_array = []
+  array.each {|x| new_array << "#{x}s"}
+  new_array
+end
